7f5df7b83aedba6e46b45a4d3456be0f36072f34
Oshima Tlholoe <ptlholoe@fnb.co.za>
2018-02-05 05:40:27 +0200

    Added Services and Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,25 @@ import {FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
 
+import {HeroService} from './hero.service';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
-  ],
-  providers: [],
+    FormsModule,
+    AppRoutingModule
+    ],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
